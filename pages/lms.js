589f8a3928224cd71e3ea56b4006ef7e1b532787
Destiny Young <155973440+youngdestinya@users.noreply.github.com>
2026-09-20 20:20:48 +0100

    Link main header to full LMS design

--- pages/lms.js
+++ pages/lms.js
@@ -1,1 +1,12 @@
-export { default } from './dashboard';
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/lms.html',
+      permanent: false,
+    },
+  };
+}
+
+export default function LmsRedirect() {
+  return null;
+}
